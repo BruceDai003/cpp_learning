0a859910cb617aa437f44b9a650f19f13bd62e90
Liwei Dai <liwei.dai@iluvatar.ai>
2021-05-08 17:20:23 +0800

    try: separte template function 'compare' declaration and definition

--- chap16/compare.cpp
+++ chap16/compare.cpp
@@ -3,29 +3,15 @@
  * @Author: Liwei Dai
  * @Date: 2021-05-08 11:43:53
  * @LastEditors: VSCode
- * @LastEditTime: 2021-05-08 11:54:34
+ * @LastEditTime: 2021-05-08 15:06:45
  */
 
-#include <iostream>
-#include <vector>
 #include "compare.h"
 
-using std::vector;
-using std::string;
-int main()
+template <typename T>
+int compare(const T &v1, const T &v2)
 {
-    int result1, result2, result3, result4;
-    result1 = compare(1, 5);
-    result2 = compare(3.5, 2.0);
-    string s1{"Liwei"}, s2{"LeBron"};
-    result3 = compare(s1, s2);
-    vector<int> vec1{1, 2, 3};
-    vector<int> vec2{1, 6, 0};
-    result4 = compare(vec1, vec2);
-
-    std::cout << "Comparing ints\t" << result1 << std::endl;
-    std::cout << "Comparing doubles\t" << result2 << std::endl;
-    std::cout << "Comparing strings\t" << result3 << std::endl;
-    std::cout << "Comparing vectors\t" << result4 << std::endl;
-    
+    if (v1 < v2) return -1;
+    if (v2 < v1) return 1;
+    return 0;
 }
--- chap16/compare.h
+++ chap16/compare.h
@@ -1,7 +1,7 @@
 /*
  * @Author: Liwei Dai
  * @Date: 2021-05-08 11:23:28
- * @LastEditTime: 2021-05-08 14:55:37
+ * @LastEditTime: 2021-05-08 17:17:00
  * @LastEditors: VSCode
  * @Description: 
  * @FilePath: \cpp_learning\chap16\compare.h
@@ -10,11 +10,6 @@
 #define COMPARE_H
 
 template <typename T>
-int compare(const T &v1, const T &v2)
-{
-    if (v1 < v2) return -1;
-    if (v2 < v1) return 1;
-    return 0;
-}
+int compare(const T &v1, const T &v2);
 
 #endif
